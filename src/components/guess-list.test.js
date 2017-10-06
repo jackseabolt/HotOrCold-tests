3483c2cb0f8e896d2ee6e3f236bc7e9458a80fdd
Jack Seabolt <jackseabolt@gmail.com>
2017-10-06 12:04:32 -0700

    guess-list tests complete

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -7,5 +7,17 @@ describe('<GuessList /> component', () => {
     it('Smoke test', () => {
         const myArr = []; 
         shallow(<GuessList guesses={myArr} />); 
-    })
-})
+    }); 
+
+    it('Renders a ul element with class of guessBox', () => {
+        const myArr = []; 
+        const wrapper = shallow(<GuessList guesses={myArr} />); 
+        expect(wrapper.hasClass('guessBox')).toEqual(true); 
+    });
+
+    it('Renders the data contained in props.guesses into the DOM', () => {
+        const myArr = ['guess1']; 
+        const wrapper = shallow(<GuessList guesses={myArr} />); 
+        expect(wrapper.contains(<li key="0">guess1</li>)).toEqual(true);
+    });
+});
