e633ae5e68f4577d399c39a5f19a65155dbcbfb7
Jack Seabolt <jackseabolt@gmail.com>
2017-10-06 11:54:25 -0700

    guess-form tests complete

--- src/components/guess-form.test.js
+++ src/components/guess-form.test.js
@@ -6,10 +6,20 @@ import GuessForm from './guess-form';
 describe('<GuessForm /> component', () => {
     it('Smoke test', () => {
         shallow(<GuessForm />); 
-    })
+    }); 
 
     it('Renders a form', () => {
         const wrapper = shallow(<GuessForm />); 
         expect(wrapper.type()).toEqual('form')
-    })
+    });
+
+    it('Should fire method in onGuess prop when form submits', () =>{
+        const callback = jest.fn(); 
+        const value = "testvalue"; 
+        const wrapper = mount(<GuessForm onGuess={callback} />); 
+        wrapper.find('input[type="text"]').node.value = value; 
+        wrapper.simulate('submit'); 
+        expect(callback).toHaveBeenCalledWith(value); 
+    });
+    
 })
