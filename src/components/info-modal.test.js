df314bffda4c0f96126831f84477d3364387a40b
ericpcho <ericpcho@gmail.com>
2017-10-06 13:48:36 -0700

    finished testing for info-modal.test file

--- src/components/info-modal.test.js
+++ src/components/info-modal.test.js
@@ -7,4 +7,23 @@ describe('<InfoModal /> component', () => {
     it('Smoke test', () => {
         shallow(<InfoModal/>); 
     })
+
+    it ('Renders a div', () => {
+        const wrapper = shallow(<InfoModal />);
+        expect(wrapper.type()).toEqual("div")
+    })
+
+    it('Displays class name overlay', () => {
+        const wrapper = shallow(<InfoModal />); 
+        expect(wrapper.hasClass('overlay')).toEqual(true); 
+    });
+
+    it('Runs onClose with props being passed through', () => {
+        const callback = jest.fn();
+        const prevDef= {preventDefault: function(){}} 
+        const wrapper = shallow(<InfoModal onClose={callback}/>); 
+        wrapper.find('a[className="close"]').simulate('click', prevDef);
+        expect(callback).toHaveBeenCalled();
+     })
+
 })
