fe30ef5e46e1d5ec48ddc3cd1e206aed8ca25fed
ericpcho <ericpcho@gmail.com>
2017-10-06 13:54:10 -0700

    finished testing for top-nav.test

--- src/components/top-nav.test.js
+++ src/components/top-nav.test.js
@@ -7,4 +7,25 @@ describe('<TopNav /> component', () => {
     it('Smoke test', () => {
         shallow(<TopNav />); 
     })
+
+    it ('Renders a nav', () => {
+        const wrapper = shallow(<TopNav />);
+        expect(wrapper.type()).toEqual("nav")
+    })
+
+    it('Runs OnNewGame with props being passed through', () => {
+        const callback = jest.fn();
+        const prevDef= {preventDefault: function(){}} 
+        const wrapper = shallow(<TopNav onNewGame={callback}/>);
+        wrapper.find('a[className="new"]').simulate('click', prevDef);
+        expect(callback).toHaveBeenCalled();
+     })
+
+     it('Runs onInfo with props being passed through', () => {
+        const callback = jest.fn();
+        const prevDef= {preventDefault: function(){}} 
+        const wrapper = shallow(<TopNav onInfo={callback}/>);
+        wrapper.find('a[className="what"]').simulate('click', prevDef);
+        expect(callback).toHaveBeenCalled();
+     })
 })
