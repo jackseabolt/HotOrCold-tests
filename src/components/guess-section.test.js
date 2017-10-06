731995116b04d6fcd0ac02967db76711a3a97382
ericpcho <ericpcho@gmail.com>
2017-10-06 13:10:51 -0700

    added tests for header.test.js

--- src/components/guess-section.test.js
+++ src/components/guess-section.test.js
@@ -7,4 +7,15 @@ describe('<GuessSection /> component', () => {
     it('Smoke test', () => {
         shallow(<GuessSection />); 
     })
+
+    it('Renders a Section', () => {
+        const wrapper = shallow(<GuessSection />)
+        expect(wrapper.type()).toEqual("section")
+    })
+
+    it('Renders a h2 with props passed into it', () => {
+        const prop = "random"
+        const wrapper = shallow(<GuessSection feedback={prop}/>)
+        expect(wrapper.contains(<h2 id="feedback">{prop}</h2>))
+    })
 })
--- src/components/header.test.js
+++ src/components/header.test.js
@@ -1,10 +1,41 @@
 import React from 'react'; 
 import { shallow, mount } from 'enzyme'; 
 
-import Header from './header'; 
+import Header from './header';
+import InfoModal from './info-modal'
 
 describe('<Header /> component', () => {
     it('Smoke test', () => {
         shallow(<Header />); 
+    })
+
+    it('Displays header tag', () => {
+        const wrapper = shallow(<Header />)
+        expect(wrapper.type()).toEqual('header')
+    })
+
+    it('renders with state as infomodal false', () => {
+        const wrapper = shallow (<Header />)
+        expect(wrapper.state('showInfoModal')).toEqual(false);
+    })
+
+    it ('changes the state of InfoModal to True when the function runs', () => {
+        const wrapper = shallow(<Header />);
+        wrapper.instance().toggleInfoModal();
+        expect(wrapper.state('showInfoModal')).toEqual(true);
+    })
+
+    it ('displays the correct information when show InfoModal is set to true', () => {
+        const wrapper = shallow(<Header />);
+        return (
+        new Promise ( function(resolve, reject) {wrapper.setState({showInfoModal: true}, resolve )})
+        .then(() => {
+            expect(wrapper.containsMatchingElement(<InfoModal />)).toEqual(true)
+        }))
+    })
+
+    it ('displays the correct information when show InfoModal is set to true', () => {
+        const wrapper = shallow(<Header />);
+        expect(wrapper.containsMatchingElement(<InfoModal />)).toEqual(false)
     })
 })
